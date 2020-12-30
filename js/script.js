6cc8c3ee570e538918519e7ce8676514db715db7
janxavierv <37735268+janxavierv@users.noreply.github.com>
2020-12-30 21:01:27 +1100

    Meet expectations
    I've managed to work out the code and this should achieve "Meet expectations"

--- js/script.js
+++ js/script.js
@@ -17,19 +17,18 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-const perPage = 10;
+const perPage = 9;
 let studentList = document.querySelector(".student-list");
 
-
 let showPage = (arrayObjects, page) => {
-  let html;
+  studentList.innerHTML = ""
   let startIndex = (page * perPage) - perPage;
   let endIndex = (page * perPage);
+  studentList.innerHTML = "";
   for (let i = 0; i < arrayObjects.length; i++){
     if(i >= startIndex && i < endIndex){
-
       let items  = arrayObjects[i]
-       html += `
+      let html = `
       <li class="student-item cf">
       <div class="student-details">
       <img class="avatar" src="${items.picture.thumbnail}" alt="Profile Picture">
@@ -40,8 +39,9 @@ let showPage = (arrayObjects, page) => {
       <span class="date">Joined ${items.registered.date}</span>
       </div>
       </li>`
-      studentList.innerHTML = html;
+      studentList.insertAdjacentHTML("beforeend", html)
     }
+
   }
 }
 /*
@@ -49,7 +49,42 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+let ul = document.querySelector(".link-list")
+let li = document.getElementsByTagName("li")
+
+let addPagination = (list) => {
+  const numOfButton = Math.ceil(list.length / perPage);
+  const ul = document.querySelector("ul.link-list");
+
+  ul.innerHTML = '';
+  for(let i = 1; i <= numOfButton; i++){
+    let buttons = `<li><button type="button">${i}</li>`
+    ul.insertAdjacentHTML("beforeend", buttons)
+    // let li = document.createElement("li");
+    // let button = document.createElement("button");
+    // button.type = "button";
+    // ul.append(li)
+    // li.append(button)
+    // button.textContent = i;
+  }
+
+  let firstButton = document.querySelector("button");
+  firstButton.className = "active";
+
+  // Bubbling, delegating the event listener to the parent tag element.
+  ul.addEventListener("click", e =>{
+    if(e.target.tagName === "BUTTON"){
+      let buttonActive = document.querySelector(".active");
+      buttonActive.className = ""
+      e.target.className = "active"
+      showPage(list, e.target.textContent)
+    }
+  })
+  console.log(firstButton)
+}
+
 
 
 // Call functions
-showPage(data,1);
+showPage(data,1)
+addPagination(data);
